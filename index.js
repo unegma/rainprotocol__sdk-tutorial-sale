c439d34a50e87282411b181102a1a5b3159e03be
Timothy Coleman <timhc22@gmail.com>
2022-04-26 18:26:08 +0100

    Tweak

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ export async function saleExample() {
     console.log(result); // the Sale contract and corresponding address
 
     // extra functionality
-    console.log(result.address);
+    console.log(`Getting address: ${result.address}`);
     let saleContract = new rainSDK.Sale(result.address, signer);
     console.log(saleContract);
     let price = await saleContract.calculatePrice(100);
