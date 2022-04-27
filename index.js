e3bcc79a5234e582bc9a0295e20161ce83923b17
Timothy Coleman <timhc22@gmail.com>
2022-04-27 20:14:26 +0100

    Change and tidy code

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 import * as rainSDK from "rain-sdk";
 import { ethers} from "ethers";
 
+// tutorial: https://docs.rainprotocol.xyz/guides/SDK/using-the-rain-sdk-to-deploy-a-sale-example-with-opcodes/
 export async function saleExample() {
-
-  const CHAIN_ID = 80001;
+  const CHAIN_ID = 80001; // Mumbai testnet chain id
+  const erc20decimals = 18; // See here for more info: https://docs.openzeppelin.com/contracts/3.x/erc20#a-note-on-decimals
+  const staticPrice = ethers.utils.parseUnits("100", erc20decimals);
+  const walletCap = ethers.utils.parseUnits("10", erc20decimals);
   const saleState = {
     canStartStateConfig: undefined,
     canEndStateConfig: undefined,
     calculatePriceStateConfig: undefined,
     recipient: "",
     reserve: "0x25a4dd4cd97ed462eb5228de47822e636ec3e31a",
-    saleTimeout: 100,
-    cooldownDuration: 100,
-    minimumRaise: 1000,
-    dustSize: 0
+    saleTimeout: 100, // this will be 100 blocks
+    cooldownDuration: 100, // this will be 100 blocks
+    minimumRaise: ethers.utils.parseUnits("1000", erc20decimals),
+    dustSize: ethers.utils.parseUnits("0", erc20decimals),
   };
   const redeemableState = {
     erc20Config: {
       name: "Raise token",
       symbol: "rTKN",
       distributor: "0x0000000000000000000000000000000000000000",
-      initialSupply: 1000,
+      initialSupply: ethers.utils.parseUnits("1000", erc20decimals),
     },
     tier: "0xC064055DFf6De32f44bB7cCB0ca59Cbd8434B2de",
     minimumTier: 0,
@@ -69,19 +72,13 @@ export async function saleExample() {
       argumentsLength: 0,
     };
 
-    // current buy units: amount want to buy, put into stack
-    // token address
-    // returns current token balance
-
     // define the parameters for the VM which will be used whenever the price is calculated, for example, when a user wants to buy a number of units
-    // the order is important
-    //
     saleState.calculatePriceStateConfig = {
-      constants: [100, 10, ethers.constants.MaxUint256], // staticPrice, walletCap, MaxUint256 (ffff..) todo check if staticPrice/walletCap needs to be parsed (divide by 18 0s?)
+      constants: [staticPrice, walletCap, ethers.constants.MaxUint256], // staticPrice, walletCap, (0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff)
       sources: [
         ethers.utils.concat([
           // put onto the stack, the amount the current user wants to buy
-          rainSDK.VM.op(rainSDK.Sale.Opcodes.CURRENT_BUY_UNITS), //
+          rainSDK.VM.op(rainSDK.Sale.Opcodes.CURRENT_BUY_UNITS),
 
           // put onto the stack, the current token balance of the user (the Sale's rTKN represented in the smart contract)
           rainSDK.VM.op(rainSDK.Sale.Opcodes.TOKEN_ADDRESS),
@@ -110,11 +107,6 @@ export async function saleExample() {
       argumentsLength: 0,
     };
 
-    // convert the initial supply into the correct format
-    redeemableState.erc20Config.initialSupply = ethers.utils.parseUnits(
-      redeemableState.erc20Config.initialSupply.toString()
-    );
-
     saleState.recipient = address;
 
     // ^-- Configuration code above this line --^
@@ -125,19 +117,16 @@ export async function saleExample() {
       redeemableState
     );
 
-    const result = await rainSDK.Sale.deploy(
+    const saleContract = await rainSDK.Sale.deploy(
       signer,
       saleState,
       redeemableState
     );
 
-    console.log(result); // the Sale contract and corresponding address
+    console.log('Sale Contract:', saleContract); // the Sale contract and corresponding address
 
     // extra functionality
-    console.log(`Getting address: ${result.address}`);
-    let saleContract = new rainSDK.Sale(result.address, signer);
-    console.log(`New Sale Contract Class:`, saleContract);
-    let price = await saleContract.calculatePrice(100);
+    let price = await saleContract.calculatePrice(ethers.utils.parseUnits("100", erc20decimals)); // todo should it be? ethers.utils.parseUnits("100", erc20decimals)
     console.log(`Price: ${price}`);
 
   } catch (err) {
